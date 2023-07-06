c39663784f8904b72140fefd026bea9535ab7046
AnnaHort <109817440+AnnaHort@users.noreply.github.com>
2023-07-06 21:35:53 +0000

    Deploying to gh-pages from @ AnnaHort/goit-js-hw-11@57dbdd51b540f6dbc71411aaa4b1fb9b0825c118 🚀

--- index.47b50c0b.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector('[name="searchQuery"]');fetch(`https://pixabay.com/api/?key=38070095-9e6d535b4dc883ad57627f805&q=${e.value}&image_type=photo&orientation=horizontal&safeswarch=true`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((e=>{console.log(e)})).catch((e=>console.log(e.status)));
-//# sourceMappingURL=index.47b50c0b.js.map
---
+++ index.f8ba2b20.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".search-form");document.querySelector(".search-button"),document.querySelector(".load-more"),document.querySelector(".gallery"),e.firstElementChild;e.addEventListener("submit",(e=>{e.preventDefault()}));
+//# sourceMappingURL=index.f8ba2b20.js.map
